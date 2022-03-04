3c9091066abfb7f6354bd2e24a28b869bce93fed
Aruna Behere <arunabehere@gmail.com>
2022-03-04 22:18:39 +0530

    first

---
+++ src/main/java/com/qa/test/App.java
@@ -0,0 +1,11 @@
+package com.qa.test;
+
+public class App {
+	String URL = "https://reqres.in";
+	String getAPI1 = "/api/unknown/2";
+	
+	
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
